99d2c72eb88b7f861d8126408cf524ebbefc1d33
Erkin Jash <shamil.erkenov@mail.ru>
2021-03-07 11:58:39 +0300

    uncommented second test

--- test_main_page.py
+++ test_main_page.py
@@ -12,9 +12,9 @@ def test_guest_can_go_to_login_page(browser):
     page2.should_be_login_form()
     page2.should_be_register_form()
 
-#def test_guest_should_see_login_link(browser):
-#    link = " http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-#    page = MainPage(browser, link)
-#    page.open()
-#    page.should_be_login_link()
+def test_guest_should_see_login_link(browser):
+    link = " http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
